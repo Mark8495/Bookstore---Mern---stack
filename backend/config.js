33d966a41a0ec964084980c9987676f7e53e6ffb
Mark Byrne <markbyrne8495@gmail.com>
2024-04-18 18:58:12 +0100

    added mongodb and mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+
+export const mongoDBURL = 'mongodb+srv://markbyrne477:Vcij3kR9VzMqJQ64@mern-book-store.7egg9i7.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Mern-Book-Store'
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -8,6 +9,16 @@ app.get('/', (request, response) => {
     return response.status(234).send("Welcome to the Mern Bookstore")
 });
 
-app.listen(PORT, () => {
-    console.log(`App is listening to: ${PORT}`)
-});
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected');
+        app.listen(PORT, () => {
+            console.log(`App is listening to: ${PORT}`)
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
